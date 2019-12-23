d8d2b72c3a850da7ce57f9f58b1e909293ad70bb
heunyam <tpwns0702@naver.com>
2019-12-23 20:00:01 +0900

    editor setting

---
+++ Front/editor/editor.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('#summernote').summernote();
+});
+
+$('#summernote').summernote({
+    height: 500, 
+    minHeight: null,
+    maxHeight: 700,
+    focus: true
+});
+
