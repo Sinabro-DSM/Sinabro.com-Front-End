d13e2aebb7b020a89edcbc1eb47901fa388f288d
heunyam <tpwns0702@naver.com>
2019-12-26 17:19:05 +0900

    editor 생성

--- Front/editor/editor.js
+++ Front/editor/editor.js
@@ -1,11 +1,7 @@
-$(document).ready(function() {
-    $('#summernote').summernote();
-});
+const submitButton = document.querySelector("#submit-button");
 
-$('#summernote').summernote({
-    height: 500, 
-    minHeight: null,
-    maxHeight: 700,
-    focus: true
-});
+submitButton.addEventListener('click', post);
+
+http.open("POST", "10.156.147.200:8000/data", true);
+http.setRequestHeader('Content-type', "x-access-token");
 
