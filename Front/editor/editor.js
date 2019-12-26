c69eb54c6ec8ec9d2ae21f9ce061fb480542af05
heunyam <tpwns0702@naver.com>
2019-12-26 20:19:37 +0900

    editor 생성

--- Front/editor/editor.js
+++ Front/editor/editor.js
@@ -1,7 +1,24 @@
 const submitButton = document.querySelector("#submit-button");
+const btn = document.querySelector("#get-button");
 
-submitButton.addEventListener('click', post);
 
-http.open("POST", "10.156.147.200:8000/data", true);
-http.setRequestHeader('Content-type', "x-access-token");
+submitButton.addEventListener('click', () => {
+    const fd = new FormData();
+    fd.append("title", "title");
+    fd.append("content", "content");
+    fd.append("img", btn.files[0]);
+    fd.append("category", "category");
+
+    axios.post("http://10.156.147.200:8000/data", fd, {
+        headers: {
+            "Content-Type": "multipart/form-data",
+        },
+    }).then(res => {
+        console.log("성공");
+    }).catch(res => {
+        console.log(res);
+        console.log("안됀다우, 동무");
+    })
+});
+
 
